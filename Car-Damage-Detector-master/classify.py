e15c48b28419b1ba017f6ac479e5238ed5ec4e0d
ArhanKhan98 <arhansam786@gmail.com>
2021-09-20 16:17:16 +0530

    Code updated for tensor flow

--- Car-Damage-Detector-master/classify.py
+++ Car-Damage-Detector-master/classify.py
@@ -1,28 +1,30 @@
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import sys
 import os
 import urllib
 
 # Disable tensorflow compilation warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import tensorflow as tf
+
 
 def prediction(image_path):
     # Read the image_data
-    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+    image_data = tf.io.gfile.GFile(image_path, 'rb').read()
     print(image_path)
 
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line
-                       in tf.gfile.GFile(r"./models/tf_files/retrained_labels.txt")]
+                       in tf.io.gfile.GFile(r"./models/tf_files/retrained_labels.txt",mode='r')]
 
     # Unpersists graph from file
-    with tf.gfile.FastGFile(r"./models/tf_files/retrained_graph.pb", 'rb') as f:
-        graph_def = tf.GraphDef()
+    with tf.io.gfile.GFile(r"./models/tf_files/retrained_graph.pb", 'rb') as f:
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
 
-    with tf.Session() as sess:
+    with tf.compat.v1.Session() as sess:
         # Feed the image_data as input to the graph and get first prediction
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
